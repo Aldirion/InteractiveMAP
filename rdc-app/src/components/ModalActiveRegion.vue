c8e25b8ff08580ec6ac26d8d5c3d8287f305a5e6
Михаил Соколов <sokol.m00@gmail.com>
2024-02-11 22:32:46 +0700

    ModalActiveRegion - RegionDashboard v1.0
    Разработан шаблон дэшборда региона. Наполнен базовыми данными

--- rdc-app/src/components/ModalActiveRegion.vue
+++ rdc-app/src/components/ModalActiveRegion.vue
@@ -1,42 +1,11 @@
-<template>
-  <Transition name="modal">
-	<div class="modal-mask">
-		<h3 class="modal-r-h">
-			{{ region.title }}
-			<button class="material-symbols-outlined" @click="closeModal">
-				close
-			</button>
-		</h3>
-		<div class="modal-r">	
-			<div class="modal-r-map">
-				<img :src="svgURL"  style="fill:black; width:100%">
-				<!-- <RU_CR ref="svg" style="width:100%"></RU_CR> -->
-			</div>
-			<div class="modal-r-container">
-				
-				
-				<div class="modal-r-indicators">
-					some text
-					{{ region.code }}
-				</div>
-				<div class="modal-r-body" @click.stop>
-					some body
-				</div>
-				<div class="modal-r-footer">
-					<!-- <button @click="closeModal">fsdi</button> <button>ghbjgdfkjh</button> -->
-				</div>
-			</div>
-		</div>
-	</div>
-  </Transition>
-</template>
-
 <script>
-// import RU_CR from './regions/RU-CR.vue'
+// import Indicator from './cards/Indicator.vue'
+import BarChart from './BarChart.vue';
+import DonughtChart from './DonughtChart.vue';
+import LineChart from './LineChart.vue';
 export default {
 	props: {
 		region: {type: Object, default: () => ({title: ""})},
-		// show: {type: Boolean, default: () => true}
 	},
 	data() {
 		return {
@@ -47,8 +16,8 @@ export default {
 	methods:
 	{
 		closeModal() {
-      		this.$emit('close');
-    	},
+					this.$emit('close');
+			},
 	},
 	computed: {
 		x() {
@@ -66,10 +35,166 @@ export default {
 		this.height = this.$el?.offsetHeight
 		console.log("mounted", this.region, " || ", this.height)
 	},
-	// components:{RU_CR}
+	components: {BarChart, DonughtChart, LineChart}
 }
 
+
 </script>
+
+
+
+<template>
+	
+	<Transition name="modal">
+		<div class="modal-mask">
+			<h1>
+				{{ region.title }}
+				<button class="material-symbols-outlined" @click="closeModal">
+					close
+				</button>
+			</h1>
+			<div class="modal-r" style="flex-wrap: nowrap; margin-bottom: 20px;">	
+				<div class="modal-r-map">
+					<img :src="svgURL" style=" ">
+				</div>
+				<div class="modal-r-info">
+					<div class="modal-r-container" style="width:100%">
+						<h1>Общая информация региона</h1>
+						<h2>Столица: г. Красноярск</h2>
+						<h3>Количество школ: 1100 Количество СПО: 72</h3>
+						
+						<!-- <BarChart /> -->
+						<div class="modal-r-container" >
+							<DonughtChart />
+							<!-- <LineChart /> -->
+						</div>
+						
+					</div>
+					<!-- <Bar :data="chartData" :options="chartOptions" /> -->
+				</div>
+				<!-- <div class="modal-r-card">
+					<div class="modal-r-container">
+						<h1>Общая информация региона</h1>
+						<h2>г. Красноярск</h2>
+						<h3>Количество школ: 1100 Количество СПО: 72</h3>
+					</div>
+				</div> -->
+				
+				
+			</div>
+			<div class="modal-r">
+				<div class="modal-r-card">
+					<div class="modal-r-h">Региональная команда</div>
+						<div class="modal-r-container"><div class="modal-r-indicator">
+							710
+						</div>
+						<h2 style="text-align: center;">Сотрудников</h2>
+					</div>	
+				</div>
+				<div class="modal-r-card">
+					<div class="modal-r-h">Воспитательные пространства и объединения</div>
+						<div class="modal-r-container"><div class="modal-r-indicator">
+							2717
+						</div>
+						<h2 style="text-align: center;">Воспитательных пространств и объединений</h2>
+					</div>	
+				</div>
+				<div class="modal-r-card">
+					<div class="modal-r-h">Повышение квалификации</div>
+					<div class="modal-r-container">
+						<div class="modal-r-indicator">
+							630
+						</div>
+						<h2 style="text-align: center;">Сотрудников, прошедших обучение по программам повышения квалификации</h2>
+					</div>
+				</div>
+			</div>
+			<h1>Проекты Росдетцентра</h1>
+			<div class="modal-r">
+				<div class="modal-r-card">
+					<div class="modal-r-h">Навигаторы детства</div>
+					<div class="modal-r-container">
+						<div class="modal-r-indicator">
+							1254
+						</div>
+						<h2 style="text-align: center;">Участников конкурса</h2>
+					</div>
+				</div>
+				<div class="modal-r-card">
+					<div class="modal-r-h">Лига Вожатых</div>
+					<div class="modal-r-container">
+						<div class="modal-r-indicator">
+							72
+						</div>
+						<h2 style="text-align: center;">Участника конкурса</h2>
+					</div>
+				</div>
+				<div class="modal-r-card">
+					<div class="modal-r-h">Добро не уходит на каникулы</div>
+					<div class="modal-r-container">
+						<div class="modal-r-indicator">
+							130
+						</div>
+						<h2 style="text-align: center;">Заявок на грантовый конкурс</h2>
+					</div>
+				</div>
+				<div class="modal-r-card">
+					<div class="modal-r-h">Орлята России</div>
+					<div class="modal-r-container">
+						<div class="modal-r-indicator">
+							20000
+						</div>
+						<h2 style="text-align: center;">Орлят</h2>
+					</div>
+				</div>
+				<div class="modal-r-card">
+					<div class="modal-r-h">Орлята России</div>
+					<div class="modal-r-container">
+						<div class="modal-r-indicator">
+							20000
+						</div>
+						<h2 style="text-align: center;">Орлят</h2>
+					</div>
+				</div>
+			</div>
+			<h1>Мероприятия календарного плана воспитательной работы</h1>
+			<div class="modal-r">
+				
+
+				<div class="modal-r-container" style="width:100%">
+					<LineChart />
+					<!-- <div class="modal-r-indicator">
+						График
+					</div> -->
+				</div>
+				<div class="modal-r-container">
+					<!-- <div class="modal-r-indicator">
+						Календарь
+					</div> -->
+					
+				</div>
+				<!-- <div>fdjsglk</div> -->
+			</div>
+			<h1>Партнеры</h1>
+			<div class="modal-r">
+				<!-- <div class="modal-r-container">
+					<h1>Партнеры</h1>
+					<div class="modal-r-indicator">
+						10
+					</div>
+				</div> -->
+				<!-- <div>fdjsglk</div> -->
+				<img style="padding:5px" src="/partners/unarmy.png">
+				<img style="padding:5px" src="/partners/unarmy.png">
+				<img style="padding:5px" src="/partners/unarmy.png">
+				<img style="padding:5px" src="/partners/unarmy.png">
+				<img style="padding:5px" src="/partners/unarmy.png">
+				<!-- <img style="padding:5px" src="/partners/unarmy.png"> -->
+			</div>	
+		</div>
+	</Transition>
+</template>
+
 
 
 <style>
@@ -77,71 +202,90 @@ export default {
 	max-width: 1280px;
 	position: absolute;
 	top:0;
-	/* z-index: 9998; */
-	/* margin: 0 auto; */
-	/* left: calc(var(--section-gap) / 2); */
 	width: 100%;
 	height: 100%;
-	/* background-color: rgba(172, 220, 253, 0.7); */
-	background-color: #fff;
-	
-	/* display:inline-block; */
-	transition: opacity 1s ease;
-	/* align-content: center; */
-	
+	background: var(--color-background);
 }
 
 .modal-r {
-	position:flex;
+	/* position:flex; */
+	display:flex;
+	flex-wrap: wrap;
 	max-width: 1280px;
+	/* gap: 2rem 1rem; */
+	/* margin:20px 10px; */
+	/* gap: 1rem 1rem; */
+	gap: 30px calc(3rem - 8px);
 	width: 100%;
 	border-radius: 2px;
 	transition: all 0.7s ease;
-	/* display:grid;
-	grid-template-columns: 1fr 2fr; */
-	display:flex;
 }
 
 .modal-r-map{
-	scale: auto;
-	/* position: relative; */
+	/* scale: auto; */
+	/* width:100%; */
+	max-width: 400px;
+	width:100%;
 	place-items: center;
 	display:inline-block;
-	margin: 10px;
+	gap: 1rem 2rem;
 	padding: 10px;
-	/* height: 100%; */
-	width: 30%;
-	/* background-color: #212121; */
+}
+
+.modal-r-info{
+	display:flex;
+	flex-grow: 2;
+	/* gap: 10px; */
+	gap: 30px calc(10rem - 10px);
+	/* background: var(--color-background-mute); */
+}
+
+.modal-r-card{
+	display:block;
+	flex-direction: column;
+	max-width: 400px;
+	width: 100%;
+	/* margin: 20px 5px; */
+	border-radius: 2px;
+	background: var(--color-background-soft);
 }
 
 .modal-r-container {
-  width: 100%;
-  /* margin: auto; */
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  border-left: 2px solid var(--color-border);
-  /* box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33); */
-  transition: all 0.3s ease;
+	padding: 20px 10px;
 }
 
+
+
 .modal-r-h {
-  margin: auto;
-  padding: 5px;
-  /* width: 100%; */
-  color: #fff;
-  font-weight: bold;
-  background-color: rgba(98, 94, 195, 1);
+	margin: 0 auto;
+	/* display: inline-block; */
+	padding: 10px;
+	/* width: 100%; */
+	color: var(--color-text);
+	text-align: center;
+	font-weight: bold;
+	font-size: 1rem;
+	background: var(--color-background-mute);
 }
-.modal-r-indicators {
-  margin-top: 0;
-  color: #42b983;
-  /* background-color: #212121; */
+.modal-r-indicator {
+	margin-top: 0;
+	display: table;
+	/* position: relative; */
+	width: 100%;
+	color: rgba(149, 145, 253, 1);
+	text-align: center;
+	font-weight: bold;
+	font-size: 5rem;
+	/* font-size: 50pt; */
+	
+	/* position: absolute; */
+
+
 }
 
 .modal-r-body {
-  margin: 20px 0;
-  color: #333;
+	margin: 20px 0;
+	color: #333;
 }
 
 .material-symbols-outlined{
@@ -149,7 +293,7 @@ export default {
 	display: inline-block;
 	background-color: transparent;
 	border-color: transparent;
-	color:#fff
+	color:var(--color-text)
 }
 
 .material-symbols-outlined :hover{
@@ -157,30 +301,30 @@ export default {
 }
 
 .modal-default-button {
-  float: right;
+	float: right;
 }
 
 .modal-enter-from {
-  opacity: 0;
+	opacity: 0;
 }
 
 .modal-leave-to {
-  opacity: 0;
+	opacity: 0;
 }
 
 .modal-enter-from .modal-container,
 .modal-leave-to .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
+	-webkit-transform: scale(1.1);
+	transform: scale(1.1);
 }
 
 /* @media (min-width: 1024px) {
-  .modal-mask{
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    //text-align: center;
-  }
+	.modal-mask{
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+		//text-align: center;
+	}
 } */
 </style>
 
---
+++ rdc-app/src/components/Personnel.vue
@@ -0,0 +1,266 @@
+<script>
+// import RU_CR from './regions/RU-CR.vue'
+export default {
+	props: {
+		region: {type: Object, default: () => ({title: ""})},
+		// show: {type: Boolean, default: () => true}
+	},
+	data() {
+		return {
+			baseUrl: '/regions/',
+			height: 0,
+		}
+	},
+	methods:
+	{
+		closeModal() {
+      		this.$emit('close');
+    	},
+	},
+	computed: {
+		x() {
+			return this.width || 0
+		},
+		y() {
+			return this.height || 0
+		},
+		svgURL(){
+			return this.baseUrl + this.region.code + '.svg'
+			console
+		},
+	},
+	mounted() {
+		this.height = this.$el?.offsetHeight
+		console.log("mounted", this.region, " || ", this.height)
+	},
+	// components:{RU_CR}
+}
+
+</script>
+
+
+
+<template>
+  <Transition name="modal">
+	<div class="modal-mask">
+		<h2 class="modal-r-h">
+			{{ region.title }}
+			<button class="material-symbols-outlined" @click="closeModal">
+				close
+			</button>
+		</h2>
+		<div class="modal-r">	
+			<div class="modal-r-map">
+				<img :src="svgURL"  style=" width:100%">
+				<!-- <object :data="svgURL" type="image/svg+xml" class="rmap" width="100%"></object> -->
+				<!-- <RU_CR ref="svg" style="width:100%"></RU_CR> -->
+			</div>
+			<div class="modal-r-container">
+				
+				
+				<div class="modal-r-indicators">
+					some text
+					{{ region.code }}
+				</div>
+				<div class="modal-r-body" @click.stop>
+					some body
+				</div>
+				<div class="modal-r-footer">
+					<!-- <button @click="closeModal">fsdi</button> <button>ghbjgdfkjh</button> -->
+				</div>
+			</div>
+			<div>
+				ffdkgh
+			</div>
+		</div>
+		<div class="modal-r">	
+			<div class="modal-r-container">
+				
+				
+				<div class="modal-r-indicators">
+					some text
+					{{ region.code }}
+				</div>
+				<div class="modal-r-body" @click.stop>
+					some body
+				</div>
+				<div class="modal-r-footer">
+					<!-- <button @click="closeModal">fsdi</button> <button>ghbjgdfkjh</button> -->
+				</div>
+			</div>
+			<div class="modal-r-container">
+				
+				
+				<div class="modal-r-indicators">
+					some text
+					{{ region.code }}
+				</div>
+				<div class="modal-r-body" @click.stop>
+					some body
+				</div>
+				<div class="modal-r-footer">
+					<!-- <button @click="closeModal">fsdi</button> <button>ghbjgdfkjh</button> -->
+				</div>
+			</div>
+			<div>
+				ffdkgh
+			</div>
+		</div>
+	</div>
+  </Transition>
+</template>
+
+
+
+<style>
+.modal-mask {
+	max-width: 1280px;
+	position: absolute;
+	top:0;
+	/* z-index: 9998; */
+	/* margin: 0 auto; */
+	/* left: calc(var(--section-gap) / 2); */
+	width: 100%;
+	height: 100%;
+	/* background-color: rgba(172, 220, 253, 0.7); */
+	/* background-color: #fff; */
+	background: var(--color-background);
+	
+	/* display:inline-block; */
+	transition: opacity 1s ease;
+	/* align-content: center; */
+	
+}
+
+.modal-r {
+	position:flex;
+	max-width: 1280px;
+	width: 100%;
+	border-radius: 2px;
+	transition: all 0.7s ease;
+	/* display:grid;
+	grid-template-columns: 1fr 2fr; */
+	display:flex;
+}
+
+.modal-r-map{
+	scale: auto;
+	place-items: center;
+	display:inline-block;
+	margin: 10px;
+	padding: 10px;
+}
+.rmap [data-code]{
+	fill: rgba(149, 145, 253, 1);
+	margin: 10px;
+	padding: 10px;
+}
+.rmap [data-code] :hover{
+	fill: rgba(202, 200, 254, 1);
+}
+
+.modal-r-container {
+  width: 100%;
+  padding: 20px 30px;
+  border-radius: 2px;
+  transition: all 0.3s ease;
+}
+
+.modal-r-h {
+  margin: auto;
+  padding: 5px;
+  /* width: 100%; */
+  color: #fff;
+  font-weight: bold;
+  background-color: rgba(98, 94, 195, 1);
+}
+.modal-r-indicators {
+  margin-top: 0;
+  color: #42b983;
+  /* background-color: #212121; */
+}
+
+.modal-r-body {
+  margin: 20px 0;
+  color: #333;
+}
+
+.material-symbols-outlined{
+	float: right;
+	display: inline-block;
+	background-color: transparent;
+	border-color: transparent;
+	color:#fff
+}
+
+.material-symbols-outlined :hover{
+	background-color:rgba(255, 255, 255, 0)
+}
+
+.modal-default-button {
+  float: right;
+}
+
+.modal-enter-from {
+  opacity: 0;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from .modal-container,
+.modal-leave-to .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+/* @media (min-width: 1024px) {
+  .modal-mask{
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    //text-align: center;
+  }
+} */
+</style>
+
+
+<!-- <style>
+/* Общий стиль тултипа */
+.tooltip-hover-region
+{
+	position: absolute;
+	min-width: 300px;
+	top: 0;
+	left: 0;
+	background: #fff;
+	border-radius: 15px;
+	box-shadow: 1px 4px 10px 1px rgba(0, 0, 0, .2);
+	/* padding: 15px; */
+	color: #333;
+	display: block;
+}
+/* Хэадер тултипа */
+.tooltip-hover-region-h
+{
+	position: static;
+	min-width: 100px;
+	width: 100%;
+	background: #5044ed;
+	border-radius: 5px 5px 0 0;
+	padding: 5px;
+	color: #f5f5f5;
+	font-weight: bold;
+}
+/* Тело тултипа */
+.tooltip-hover-region-b
+{
+	position: flex;
+	min-width: 100px;
+	width: 100%;
+	background: #fff;
+	padding: 5px 10px 5px 10px;
+	color: #333;
+}
+</style> -->
