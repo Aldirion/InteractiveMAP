9a9ea9b4a11792b7725609de9d1e3103dc8fe5b0
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2024-03-29 16:27:59 +0700

    feat: add new component Header to all pages

--- rdc-app/src/App.vue
+++ rdc-app/src/App.vue
@@ -1,99 +1,19 @@
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Главная</RouterLink>
-        <!-- <RouterLink to="/about">О проекте</RouterLink> -->
-        <RouterLink to="/map">Карта</RouterLink>
-        <!-- <RouterLink to="/test">TEST</RouterLink> -->
-        <a class="theme-toggle"><span class="material-symbols-outlined">dark_mode</span></a>
-      </nav>
-    </div>
-  </header>
+  <HeaderComponent />
   <RouterView />
 </template>
 
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+  import { defineComponent } from 'vue';
+  import HeaderComponent from './components/common/HeaderComponent.vue'
 
+  export default defineComponent({
+    components: {
+      HeaderComponent,
+    }
+  })
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-  /* margin-top: 0; */
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-.theme-toggle{
-  background-color: transparent;
-  color: rgba(94, 121, 221, 1);
-  border-color: transparent;
-  border-left: 2px solid var(--color-border);
-  display: flex;
-}
-.theme-toggle:hover{
-  background-color: var(--color-border);
-  border-color: transparent;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-  display:flex;
-  align-items: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 2px solid var(--color-border);
-  color: rgba(94, 121, 221, 1);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: center;
-    /* margin-left: -1rem; */
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
---
+++ rdc-app/src/components/common/HeaderComponent.vue
@@ -0,0 +1,72 @@
+<template lang="html">
+  <header>
+      <nav class="wrapper">
+        <RouterLink to="/">Главная</RouterLink>
+        <RouterLink to="/map">Карта</RouterLink>
+      </nav>
+      <a class="theme-toggle">
+          <span class="material-symbols-outlined">dark_mode</span>
+      </a>
+  </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="css" scoped>
+  header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 5vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 0 10vw;
+    z-index: 20;
+    background-color: var(--color-background);
+  }
+
+  .wrapper {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    font-size: 18px;
+  }
+
+  nav a {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    padding: 0 2vw;
+    border-left: 2px solid var(--color-border);
+    color: rgb(183, 183, 185);
+  }
+
+  nav a:first-of-type {
+    border: 0;
+  }
+
+  .theme-toggle{
+    border-color: transparent;
+    display: flex;
+    align-items: center;
+  }
+  
+  .theme-toggle:hover{
+    cursor: pointer;
+  }
+
+  nav a.router-link-exact-active {
+    color: rgb(70, 70, 182);
+  }
+
+  nav a:hover {
+    color: rgb(94, 94, 192);
+    background-color: rgba(149, 145, 253, 0.2);
+    transition: background-color .2s linear;
+  }
+</style>
