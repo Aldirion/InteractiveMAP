534143fe8171d1af8c2faef35732f50de826e1e0
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2024-05-20 18:58:50 +0700

    fix: fix the type of data and get it from the API

--- rdc-app/src/views/RdcTeamView.vue
+++ rdc-app/src/views/RdcTeamView.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-import { useStoreRegions } from '@/store/store';
 import { onMounted, ref } from 'vue';
-import type { EmployeeTeam } from '@/interfaces/employee';
+import type { EmployeeFederalTeam, EmpoloyeeDepartments } from '@/interfaces/employee';
 import ListComponent from '@/components/common/ListComponent.vue';
 import LoaderComponent from '@/components/common/LoaderComponent.vue';
 import ListItemComponent from '@/components/common/ListItemComponent.vue';
 import router from '@/router';
+import { BASE_URL } from '@/interfaces/variables';
 
-let employeeTeam = ref<EmployeeTeam | null>(null);
+let employeeTeam = ref<EmployeeFederalTeam | null>(null);
 let isLoaded = ref(false);
 
-const store = useStoreRegions();
-
 onMounted(async () => {
-  employeeTeam.value = await store.getEmployeeByRegionCode(91);
-
+  const teamData = await fetch(`${BASE_URL}/rdc_team/`);
+  employeeTeam.value = await teamData.json();
   isLoaded.value = true;
 });
 
@@ -34,18 +32,46 @@ function routedToUserProfile(userId: number) {
         v-for="(employee, title) in employeeTeam"
         :key="title"
         :title="`${title} (${employee.count})`"
-        :team="employee"
         :opened="employee.count < 5"
       >
-        <ListItemComponent v-for="worker in employee.data" :key="worker.id" @click="routedToUserProfile(worker.user)">
+        <ListItemComponent
+          v-for="worker in employee.employees"
+          :key="worker.id"
+          @click="routedToUserProfile(worker.user)"
+        >
           <div class="item-about">
             <p class="item-name">{{ worker.lastname }} {{ worker.firstname }} {{ worker.patronymic }}</p>
             <p class="item-email">{{ worker.email }} {{ worker.region_id }}</p>
+            <p class="item-address">{{ worker.post_title }}</p>
           </div>
           <div class="img-container">
             <img :src="worker.avatar" class="item-img" alt="worker photo" />
           </div>
         </ListItemComponent>
+
+        <div v-if="Object.keys(employee.departments).length > 0">
+          <ListComponent
+            v-for="(department, title) in employee.departments as EmpoloyeeDepartments"
+            :key="title"
+            :title="`${title} (${department.count})`"
+            :opened="department.count < 6"
+          >
+            <ListItemComponent
+              v-for="worker in department.data"
+              :key="worker.id"
+              @click="routedToUserProfile(worker.user)"
+            >
+              <div class="item-about">
+                <p class="item-name">{{ worker.lastname }} {{ worker.firstname }} {{ worker.patronymic }}</p>
+                <p class="item-email">{{ worker.email }} {{ worker.region_id }}</p>
+                <p class="item-address">{{ worker.post_title }}</p>
+              </div>
+              <div class="img-container">
+                <img :src="worker.avatar" class="item-img" alt="worker photo" />
+              </div>
+            </ListItemComponent>
+          </ListComponent>
+        </div>
       </ListComponent>
     </div>
   </div>
@@ -54,6 +80,7 @@ function routedToUserProfile(userId: number) {
 <style lang="css" scoped>
 .team-container {
   min-height: 50vh;
+  padding: 0 10vw;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -64,5 +91,12 @@ function routedToUserProfile(userId: number) {
   text-align: center;
   margin: 30px 0;
   font-size: 1.5rem;
+}
+
+@media only screen and (max-width: 1050px) {
+  .team-container {
+    min-height: 50vh;
+    padding: 0 0;
+  }
 }
 </style>
